eec0a1d92cc75a525536e266008408059c85387e
Zhan Tong <zhantong94@gmail.com>
2023-03-09 16:09:56 +0800

    Exercise 7.4

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Posts = () => {
-  return <h1>Posts</h1>;
-};
+function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/posts")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Post List</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Posts;
